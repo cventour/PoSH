c5f1dbcff5a0cc2514c4e34d62d6a9f26446edbb
Christos Ventouris <ventouris@gmail.com>
2021-03-08 19:12:08 +0400

    Update JiTAuto.ps1
    Added a table at the end of the script that summarizes all the devices that got access enabled with their Public IP and Port

--- JiTAuto.ps1
+++ JiTAuto.ps1
@@ -7,13 +7,19 @@ $RG ='YOUR RESOURCE GROUP NAME'
 $PublicIP = (Invoke-WebRequest -uri 'http://ifconfig.me/ip').Content.tostring()
 [hashtable]$Ports = @{Windows=3389; Linux=22}
 
-
 Connect-AzAccount
 Set-AzContext -SubscriptionId $SubscriptionID
 $RGVMs = $(Get-AzVM -ResourceGroupName $RG)
 $JitArray = @()
+$FinalTable=@()
+$TableItem=@{}
+
 ForEach($VM in $RGVMs) {
     $OSType=(Get-AzVM -Name $VM.Name).StorageProfile.OsDisk.OsType
+    $nicName=($VM.NetworkProfile.NetworkInterfaces[0].Id.Split('/') | select -Last 1)
+    $PublicIpName =  (Get-AzNetworkInterface -ResourceGroupName $RG -Name $nicName).IpConfigurations.PublicIpAddress.Id.Split('/') | select -Last 1
+    $PublicIP = (Get-AzPublicIpAddress -ResourceGroupName $RG -Name $PublicIpName).IpAddress
+
     Write-Host '[*] Checking' $VM.Name 'state .. located in' $VM.Location
     $VMState = $(Get-AzVM -Name $VM.Name -Status).Powerstate
     if ($VMState -eq 'Info Not Available') { Write-Host '[-] Could not Start' $VM.Name '. Info not available'}
@@ -25,7 +31,13 @@ ForEach($VM in $RGVMs) {
             Write-Host "[!] Error Name " $Error[0] }
      }  
     if ($VMState -eq 'VM running') { 
-        Write-Host '[J] Adding'$VM.Name'for JIT Request on port'$Ports["$OSType"]
+        #$TableItem=@($VM.Name,$PublicIP,$Ports["$OSType"])
+        $TableItem=@{}
+        $TableItem.Add('Name',$VM.Name)
+        $TableItem.Add('IP',$PublicIP)
+        $TableItem.Add('Port',$Ports["$OSType"])
+        $FinalTable+=$TableItem
+        Write-Host '[J] Initiating JIT request for '$VM.Name' on '$PublicIp : $Ports["$OSType"] ...'' -NoNewline
         $JitResourceID = "/subscriptions/"+$SubscriptionID+"/resourceGroups/"+$RG+"/providers/Microsoft.Security/locations/"+$vm.location+"/jitNetworkAccessPolicies/default"
         $JitPolicyVm = (@{
             id="/subscriptions/"+$SubscriptionID+"/resourceGroups/"+$RG+"/providers/Microsoft.Compute/virtualMachines/"+$VM.name;
@@ -34,7 +46,8 @@ ForEach($VM in $RGVMs) {
                endTimeUtc=(Get-Date).AddHours(3) # .tostring("yyyy-MM-ddTHH:MM:SSZ")
                allowedSourceAddressPrefix=$PublicIP})})
         $JitArray=@($JitPolicyVm)
-        Start-AzJitNetworkAccessPolicy -ResourceId $JitResourceID -VirtualMachine $JitArray | Format-Table
-
-    }
+        Start-AzJitNetworkAccessPolicy -ResourceId $JitResourceID -VirtualMachine $JitArray
+        }
 }
+$FinalTable | ForEach {[PSCustomObject]$_} | Format-Table @{label='Computer';expression={$_.Name};width=20},@{label='IP';expression={$_.IP};width=20},@{label='Port';expression={$_.Port};width=10} 
+
